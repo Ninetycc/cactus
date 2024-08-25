f98310cef643a70f7a4cf69adcbecc639638b1c5
Ninetycc <pe@ipgo.me>
2024-08-25 20:30:48 +1000

    fonts-009

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -55,6 +55,7 @@ export default {
 			transitionProperty: {
 				height: "height",
 			},
+
 			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 			// @ts-expect-error
 			// Remove above once tailwindcss exposes theme type
