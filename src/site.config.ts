8f9f14d96f1efb633ca33ec4565496c776694668
Ninetycc <pe@ipgo.me>
2024-08-25 16:54:08 +1000

    links-777

--- src/site.config.ts
+++ src/site.config.ts
@@ -43,6 +43,10 @@ export const menuLinks: { path: string; title: string }[] = [
 		path: "/posts/",
 		title: "Blog",
 	},
+  {
+		path: "/links/",
+		title: "Links",
+	},
 ];
 
 // https://expressive-code.com/reference/configuration/
